7c79bf0b34d098e84ba055e466b7be449e1e1a7f
petercsnk <peter.ch.snook@gmail.com>
2022-08-14 17:24:17 +0100

    input/output error, stuck on reading sensors

--- sensors.py
+++ sensors.py
@@ -5,12 +5,21 @@ import math
 
 GPIO.setmode(GPIO.BOARD)
 
-moisture_sensor = 0
-#temphum_sensor = 1
+#moisture_sensor = 0
+temphum_sensor = 5
 #light_sensor = 2
+print("imports")
 
-def getSensor():
-    moisture = grovepi.analogRead(moisture_sensor)
+try:
+    #def getSensor():
+    print("b4 temp hum")
+    [temp,hum] = grovepi.dht(temphum_sensor, 0)
+    print("temp =", temp, "C\thumidity =", hum,"%")
+    print("after temp hum")
+
+    #print("b4 analog read")
+    #moisture = grovepi.analogRead(moisture_sensor)
+    #print("after analog read")
     #temperature = grovepi.analogRead(temphum_sensor)
     #light = grovepi.analogRead(light_sensor)
 
@@ -20,12 +29,19 @@ def getSensor():
     #r = 100000.0 * r
     #temperature = round(1.0 / (math.log(r/100000.0) / b + 1 / 298.15) - 273.15, 3)
 
-    moisture = round(float(1 - (moisture - 500) / 250), 3)
+    #moisture = round(float(1 - (moisture - 500) / 250), 3)
 
     #light = light
 
     #print("temp: ", temperature)
-    print("mois: ", moisture)
+    #print("mois: ", moisture)
     #print("light: ", light)
 
-getSensor()
+except IOError:
+    print("Error")
+#except KeyboardInterrupt:
+    #exit()
+except Exception as e:
+    print(e)
+
+#getSensor()
